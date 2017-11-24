9527ed666f7dbf93f8cbde8dc3200c001dfd7f6e
Marek <marek.skrzypecki@gmail.com>
2017-11-24 20:19:05 +0100

    sd

--- No Project Name/No Project Name/Menu.cpp
+++ No Project Name/No Project Name/Menu.cpp
@@ -1,15 +1,36 @@
 #include "stdafx.h"
 #include "Menu.h"
 
-
 Menu::Menu()
 {
+	
 }
 
+Menu::Menu(RenderWindow &window, Engine::gameState state)
+{
+	state = Engine::gameState::Exit;
+
+	if (!font.loadFromFile("Resources/Pixeled.tff"))
+	{
+		MessageBox(NULL, (LPCWSTR) "Font not found!", (LPCWSTR)  "ERROR", NULL);
+		return;
+	}
+
+	state = Engine::gameState::Menu;
+}
 
 Menu::~Menu()
 {
-}
-void Menu::Run() {
 
+}
+
+Engine::gameState Menu::Run() 
+{
+	Text title("There is no title", font, 80);
+	title.setStyle(Text::Bold);
+	title.setPosition(1280 / 2 - title.getGlobalBounds().width / 2, 20);
+
+	const short int options = 2;
+
+	Text texts[options];
 }
--- No Project Name/No Project Name/Menu.h
+++ No Project Name/No Project Name/Menu.h
@@ -1,10 +1,20 @@
 #pragma once
+
+#include <SFML/Graphics.hpp>
+#include <Windows.h>
+#include <String>
+#include "Engine.h"
+
+using namespace sf;
+using namespace std;
+
 class Menu
 {
+private:
+	Font font;
 public:
 	Menu();
+	Menu(RenderWindow &window, Engine::gameState state);
 	~Menu();
-	void Run();
+	Engine::gameState Run();
 };
-
-//http://szymonsiarkiewicz.pl/obsolete/piszemy-gre-w-sfmlu/piszemy-gre-w-sfmlu-lekcja-1/
