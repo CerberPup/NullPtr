279a2cd056be4d8f27e659b10768c6732b1ab9a8
Marek <marek.skrzypecki@gmail.com>
2017-11-24 22:30:41 +0100

    .

--- No Project Name/No Project Name/Engine.cpp
+++ No Project Name/No Project Name/Engine.cpp
@@ -7,7 +7,9 @@ Engine::Engine()
 	const HWND hDesktop = GetDesktopWindow();
 	RECT ResolutionRect;
 	GetWindowRect(hDesktop, &ResolutionRect);
-	window.create(sf::VideoMode(ResolutionRect.right,ResolutionRect.bottom), "SFML works!", sf::Style::Fullscreen);
+	window.setPosition(Vector2i(0, 0));
+	window.create(sf::VideoMode(ResolutionRect.right,ResolutionRect.bottom), "SFML works!");
+	//MessageBox(NULL, TEXT("" + ResolutionRect.right + ResolutionRect.bottom), TEXT("k"), NULL);
 	window.setFramerateLimit(60);
 	state = gameState::MENU;
 }
